b92cf1fbbda6bbf67b376de8a99d92037d6527c8
kiuuon <k41471804@gmail.com>
2024-10-17 20:13:22 +0900

    chore: reset.css 설정 및 styled-components 설치

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,18 @@
+import styled from 'styled-components';
+
+const Header = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100px;
+  background-color: #d9d9d9;
+`;
+
 function Home() {
-  return <>hello world :p</>;
+  return (
+    <div>
+      <Header></Header>
+    </div>
+  );
 }
 
 export default Home;
