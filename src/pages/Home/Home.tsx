2cdbcc180fdebe9c1eb01094e75aa10040dbb912
kiuuon <k41471804@gmail.com>
2024-10-17 20:57:41 +0900

    feat: 공지사항 레이아웃

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,19 +1,28 @@
 import styled from 'styled-components';
 
 import Header from './Header.tsx';
+import Notification from './notification.tsx';
+
+const Main = styled.div`
+  width: 100%;
+  padding: 0 30px;
+  box-sizing: border-box;
+`;
 
 const Banner = styled.div`
-  width: calc(100% - 60px);
   height: 300px;
   background-color: #d9d9d9;
-  margin: 40px 30px;
+  margin: 40px 0;
 `;
 
 function Home() {
   return (
     <div>
       <Header />
-      <Banner></Banner>
+      <Main>
+        <Banner />
+        <Notification />
+      </Main>
     </div>
   );
 }
