2c9a5a119d7ca5da3c9cf1a282a60ec6ab9e791a
kiuuon <k41471804@gmail.com>
2024-10-17 20:40:10 +0900

    feat: 배너 레이아웃 구성

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,19 @@
+import styled from 'styled-components';
+
 import Header from './Header.tsx';
+
+const Banner = styled.div`
+  width: calc(100% - 60px);
+  height: 300px;
+  background-color: #d9d9d9;
+  margin: 40px 30px;
+`;
 
 function Home() {
   return (
     <div>
       <Header />
+      <Banner></Banner>
     </div>
   );
 }
