3064f1228c908a6653b92d5993940457ab103d2a
kiuuon <k41471804@gmail.com>
2024-11-21 02:19:40 +0900

    feat: 일정 섹션 달력 UI 완성

--- src/pages/Home/ScheduleSection/ScheduleSection.tsx
+++ src/pages/Home/ScheduleSection/ScheduleSection.tsx
@@ -1,27 +1,30 @@
-import { useState } from 'react';
 import styled from 'styled-components';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 
 import AnimatedSection from '../AnimationSection.tsx';
 
-type DatePiece = Date | null;
-
-type SelectedDate = DatePiece | [DatePiece, DatePiece];
-
 function ScheduleSection() {
-  const [selectedDate, setSelectedDate] = useState<SelectedDate>(new Date());
-
   return (
     <AnimatedSection>
       <Container>
-        <Calendar onChange={setSelectedDate} value={selectedDate} />
+        <BlockCalendar />
+        <CustomCalendar
+          value={new Date()}
+          calendarType="gregory"
+          nextLabel={null}
+          prevLabel={null}
+          next2Label={null}
+          prev2Label={null}
+          tileContent={({ date }) => <span>{date.getDate().toString().padStart(2, '0')}</span>}
+        />
       </Container>
     </AnimatedSection>
   );
 }
 
 const Container = styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -30,14 +33,146 @@ const Container = styled.div`
   height: 100%;
 `;
 
-const Title = styled.div`
-  height: 50px;
-  font-size: 50px;
+const BlockCalendar = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 750px;
+  z-index: 10;
 `;
 
-const Contents = styled.div`
-  height: 450px;
-  background-color: #d9d9d9;
+const CustomCalendar = styled(Calendar)`
+  border: none;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  background-color: #ffffff;
+  width: 100%;
+  height: 730px;
+
+  .react-calendar__navigation {
+    display: flex;
+    justify-content: space-between;
+    background: var(--primary-color);
+    color: white;
+    width: 100%;
+    height: 75px;
+    border-radius: 10px 10px 0 0;
+    padding: 10px;
+
+    button {
+      color: white;
+      font-size: 24px;
+      background-color: var(--primary-color);
+      border: none;
+      cursor: pointer;
+
+      &:hover {
+        background-color: #5fa1a2;
+        border-radius: 6px;
+      }
+
+      &:focus {
+        background-color: var(--primary-color);
+        &:hover {
+          background-color: #5fa1a2;
+          border-radius: 6px;
+        }
+      }
+    }
+    span {
+      font-family: 'Pretendard', sans-serif;
+      font-size: 24px;
+      font-weight: 500;
+    }
+  }
+
+  .react-calendar__tile {
+    position: relative;
+    border: solid 1px #f0f0f0;
+    height: 120px;
+    font-family: 'Pretendard', sans-serif;
+    font-size: 16px;
+
+    abbr {
+      display: none;
+    }
+
+    &:hover {
+      background-color: aliceblue;
+    }
+
+    &:focus {
+      background-color: white;
+      &:hover {
+        background-color: aliceblue;
+      }
+    }
+
+    span {
+      font-family: 'Pretendard', sans-serif;
+      font-size: 16px;
+      position: absolute;
+      top: 11px;
+      right: 11px;
+      z-index: 2;
+    }
+  }
+
+  .react-calendar__month-view__weekdays__weekday {
+    font-family: 'Pretendard', sans-serif;
+    font-size: 16px;
+    font-weight: 500;
+    height: 40px;
+    padding-right: 10px;
+    display: flex;
+    align-items: end;
+    justify-content: end;
+  }
+
+  .react-calendar__tile--active {
+    background: white;
+
+    &:hover {
+      background-color: aliceblue;
+    }
+
+    &:focus {
+      background-color: white;
+      &:hover {
+        background-color: aliceblue;
+      }
+    }
+  }
+
+  .react-calendar__tile--active::after {
+    content: '';
+    position: absolute;
+    width: 30px;
+    height: 30px;
+    background-color: #d10000;
+    border-radius: 50%;
+    top: 5px;
+    right: 5px;
+    z-index: 0;
+  }
+
+  .react-calendar__tile--hasActive {
+    background: white;
+
+    &:hover {
+      background: white;
+    }
+    &:focus {
+      background: white;
+    }
+  }
+
+  .react-calendar__month-view__weekdays__weekday--weekend {
+    color: #d10000;
+  }
+
+  .react-calendar__month-view__days__day--neighboringMonth {
+    color: #c9c9c9;
+  }
 `;
 
 export default ScheduleSection;
